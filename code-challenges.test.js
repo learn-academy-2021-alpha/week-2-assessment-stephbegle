e788ac8bab44fa94862b9ee5354ed8db2f5b17a1
stephbegle <stephbegle@gmail.com>
2021-02-20 17:31:03 -0800

    initial commit saving work

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,19 @@
 // ASSESSMENT 2: Coding practical questions with Jest
 
+// const { expect } = require("@jest/globals");
+
+// const { describe } = require("yargs");
+
+// const { test } = require("picomatch");
+// const { describe } = require("yargs");
+
+// const { test } = require("picomatch");
+
+// const { italic } = require("ansi-styles");
+// const { exportAllDeclaration } = require("babel-types");
+// const { test } = require("picomatch");
+// const { describe } = require("yargs");
+
 // Please read all questions thoroughly
 // Pseudo coding is HIGHLY recommended
 // If you get stuck, please leave comments to help us understand your thought process
@@ -17,21 +31,56 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var num1 = 15
-// Expected output: "15 is divisible by three"
+describe("When isDivByThree is called", () => {
+    it("returns '15 is divisible by three'", () => {
+        const num = 15;
 
-var num2 = 0
-// Expected output: "0 is divisible by three"
+        const actResult = isDivByThree(num);
 
-var num3 = -7
-// Expected output: "-7 is not divisble by three"
+        expect(actResult).toEqual("15 is divisible by three")
+    })
+    it("returns '0 is divisible by three'", () => {
+        const num = 0;
 
+        const actResult = isDivByThree(num);
 
+        expect(actResult).toEqual("0 is divisible by three")
+    })
+    it("returns '-7 is not divisible by three'", () => {
+        const num = -7;
 
+        const actResult = isDivByThree(num);
 
+        expect(actResult).toEqual("-7 is not divisible by three")
+    })
+
+})
 
 // b) Create the function that makes the test pass.
 
+const isDivByThree = (num) => {
+
+    if(num % 3 === 0){
+        // if the number is evenly divisble by three print this
+        return `${num} is divisible by three`
+
+    } else {
+        // if not, print this
+        return `${num} is not divisible by three`
+    }
+};
+
+var num1 = 15
+console.log(isDivByThree(num1))
+// Expected output: "15 is divisible by three"
+
+var num2 = 0
+console.log(isDivByThree(num2))
+// Expected output: "0 is divisible by three"
+
+var num3 = -7
+console.log(isDivByThree(num3))
+// Expected output: "-7 is not divisble by three"
 
 
 
@@ -46,12 +95,34 @@ var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+describe("When capitalize is called," , () => {
+    it("will return an array of capitalized strings", () => {
+        const actResult = capitalize(randomNouns1);
 
+        expect(actResult).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+    })
+    it("will return an array of capitalized strings", () => {
+        const actResult = capitalize(randomNouns2);
 
-
+        expect(actResult).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+const capitalize = (array) => {
+
+    newArr = []
+    // initializing a new array 
+
+    for(i = 0; i < array.length; i++){
+        newArr.push(array[i].charAt(0).toUpperCase() + array[i].slice(1))
+        // add the first letter of the current element to the new array and capitalize it, then add the rest of the word (starting at index 1), to complete the word.
+    }
+
+    return newArr
+}
+console.log(capitalize(randomNouns1))
 
 
 
@@ -63,16 +134,66 @@ var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction
 var mixedDataArray1 = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 107]
 
-var mixedDataArray1 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
+var mixedDataArray2 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
 // Expected output: [-9, 0, 3, 18, 94]
 
+describe("When ascNums is called", () => {
+    it("will return an array of only numbers ordered from least to greatest", () => {
+        const mixedDataArray1 = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"];
 
+        const actResult = ascNums(mixedDataArray1);
 
+        expect(actResult).toEqual([-8, 0, 8, 46, 59, 107]);
+    })
+    it("will return an array of only numbers ordered from least to greatest", () => {
+        const mixedDataArray2 = [3, "yo!", 94, -9, false, 0, 18, "hola!"];
 
+        const actResult = ascNums(mixedDataArray2);
+        
+        expect(actResult).toEqual([-9, 0, 3, 18, 94]);
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+// this sort of works but ignores 0 so does not pass the test
+const ascNums = (array) => {
+    counter = 0;
+    return array.filter(value => {
+        if(typeof value === "number"){
+            return value
+        }
+    }).sort((a, b) => a - b)
+}
 
+// this also almost works, still working on it
+const ascNums1 = (array) => {
+
+    numArr = [];
+    sortArr = [];
+
+    for(i = 0; i < array.length; i++){
+
+        if(typeof array[i] === "number"){
+            // loop through the array and find only the nubers
+            numArr.push(array[i])
+            // push the numbers into numArr array
+        }
+
+    }for(j = 0; j < numArr.length; j++){
+
+        min = Math.min.apply(Math, numArr);
+        // find the min number in the array and set it to a variable
+        sortArr.push(min);
+        // push the min number to a new array (sortArr)
+        index = numArr.indexOf(min)
+        // find the index of the min number in the old array (numArr)
+        numArr.splice(index, 1)
+        // remove the current min number from the old array using its index
+    }
+
+    return sortArr
+}
 
 
 
@@ -87,8 +208,37 @@ var vowelTester2 = "academy"
 var vowelTester3 = "challenge"
 // Expected output: 2
 
+describe("When firstVow is called,", () => {
+    it("returns the index of the first vowel of the string 'learn'", () => {
+        const vowelTester1 = "learn";
 
+        const actResult = firstVow(vowelTester1);
 
+        expect(actResult).toEqual(1);
+    })
+    it("returns the index of the first vowel of the string 'academy'", () => {
+        const vowelTester1 = "academy";
+
+        const actResult = firstVow(vowelTester1);
+
+        expect(actResult).toEqual(0);
+    })
+    it("returns the index of the first vowel of the string 'challenge'", () => {
+        const vowelTester1 = "challenge";
+
+        const actResult = firstVow(vowelTester1);
+
+        expect(actResult).toEqual(2);
+    })
+
+})
 
 
 // b) Create the function that makes the test pass.
+
+const firstVow = (string) => {
+
+    let search = string.search(/[a,e,i,o,u,A,E,I,O,U]/g)
+    // the search method searches for the specified values (vowels) and returns the index of the first match
+    return search
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,61 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Why would you use the method super()?
 
-  Your answer:
+  Your answer: The super() method is used in child classes and calls the information made in parent classes so that the child class is able to access all of its properties. 
 
-  Researched answer:
+  Researched answer: The super keyword refers to the parent class and is used by a child class to call the constructor of the parent class and be able to access all of the parent class properties and methods. It allows new objects to be created by calling the constructor method of the parent class.
+
+  https://www.w3schools.com/jsref/jsref_class_super.asp
 
 
 
 2. What is a virtual DOM?
 
-  Your answer:
+  Your answer: DOM stands for document ordered model and a virtual DOM is a document that lives in between the website and the actual code used to create a website. The job of the virtual DOM is to look for changes the user has made, isolate those changes, and only update those changes on the page, leaving everything else in tact. This allows the page to continue to update without a refreshing of the page.
 
-  Researched answer:
+  Researched answer: The VDOM is a concept that explains the process of there being an updated change to the actual DOM. When there is a change in the user interface, that change (or VDOM) is stored in memory and then synced with the actual DOM using a library such as ReactDom (this process is called reconciliation). The VDOM is thought of as more of a pattern than a technology; it absorbs the changes on an app and updates the DOM seamlessly.   
+
+https://reactjs.org/docs/faq-internals.html
 
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to display "Hello World" in the browser?
 
-  Your answer:
+  Your answer: In order to display anything in the browser, we need to use HTML-like openning and closing tags. For example, "<h1>Hello World</h1>"
 
-  Researched answer:
+  Researched answer: The necessary elements we need are script tags and html or JSX tags. 
+
+  https://medium.com/the-andela-way/understanding-react-components-37f841c1f3bb
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JSX looks alot like HTML and is the language being used inside of script tags in React. The script tags allow us to write JavaScript code and the HTML-looking code we end up writing inside of that is actually JSX. 
 
-  Researched answer:
+  Researched answer: JSX stands for JavaScript XML and is the language that allows us to write HTML in React. It converts HTML tags into react elements. You are not required to use JSX in React but it does make it easier to write React applications because otherwise, you'd have to use methods to create actual HTML elements in React. 
+
+  https://www.w3schools.com/react/react_jsx.asp#:~:text=JSX%20stands%20for%20JavaScript%20XML.&text=JSX%20makes%20it%20easier%20to%20write%20and%20add%20HTML%20in%20React.
 
 
 
 5. What is state in React?
 
-  Your answer:
+  Your answer: state is a built-in object for React :) (: 
 
-  Researched answer:
+  Researched answer: the state is where you store the property values that belong to the component. You can manage the properties of the component within the component like you would variables defined in a function.   
+
+  https://reactjs.org/docs/faq-state.html
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: I completely wouldn't have had an answer for this one. I only remember it mentioned during testing with jest. 
 
-  Researched answer:
+  Researched answer: Hoisting is a behavior that moves declarations from the bottom to the top of the current script. Meaning you can declare a variable after you have already used it. The variable is considered to be recognized but still in a "dead zone" at the start of the script until it is declared. 
+
+  https://www.w3schools.com/js/js_hoisting.asp
 
 
 
@@ -58,7 +70,8 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-- React lifecycle methods
+- React lifecycle methods - 
+https://reactjs.org/docs/react-component.html 
 - React props
 - event.preventDefault()
 - DOM events
